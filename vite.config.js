752f3453ddb2f25e50e7c2aee4960d4ac79326bb
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-11-12 18:16:10 -0400

    instalacion y configuracion de vuetify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { createVuePlugin as vue } from 'vite-plugin-vue2'
+import Components from 'unplugin-vue-components/vite'
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
+
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [createVuePlugin()],
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [
+                // Vuetify
+                VuetifyResolver(),
+            ],
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
